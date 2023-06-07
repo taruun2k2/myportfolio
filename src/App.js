fe6649aeb4463293ce46536f6fb55747c9390458
Taruun <flashyfist@gmail.com>
2023-06-07 16:39:44 +0530

    deployed1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index  element={<Home />} />
+          <Route index path="/myportfolio" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<Portfolio />} />
